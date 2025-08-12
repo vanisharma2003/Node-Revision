09639391129f1ec4f82e5b71f354c63015874497
st-vani <137780818+vanisharma2003@users.noreply.github.com>
2025-08-12 08:17:03 +0530

    url (uniform resource locator)

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,14 @@
 const http = require("http")
+const url = require("url")
 const myCustomServer = http.createServer((req,res)=>{
-   switch(req.url){
+if(req.url === "/favicon.ico") return
+const myUrl = url.parse(req.url,true)
+console.log(myUrl)
+console.log(myUrl.query)
+   switch(myUrl.pathname){
    case "/":res.end("Welcome")
    break;
-   case "/about":res.end("Hello I am Vani sharma")
+   case "/about":res.end(`Hello ${myUrl.query.username}`)
    break;
    default:res.end("404 not found")
    break;
