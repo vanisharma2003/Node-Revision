fc141c45fe207b390049da0f952d79a07afc6746
st-vani <137780818+vanisharma2003@users.noreply.github.com>
2025-08-16 23:36:31 +0530

    middleware

---
+++ MiddleWare/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const sampleData = [{ name: "Vani" }, { name: "Aditya" }];
+app.use((req,res,next)=>{
+console.log("I am middleware")
+req.name="Meenu"
+next()
+})
+app.get("/", (req, res) => {
+  return res.json(sampleData);
+});
+app.get("/custom", (req, res) => {
+  return res.send(req.name)
+});
+app.listen(8000,()=>(
+    console.log("Server Started")
+))
